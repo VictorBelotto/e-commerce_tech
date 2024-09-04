f0443718efc16b19cdd03c5ad0c1e7a0fa7453d2
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-09-04 14:14:40 -0300

    Initial commit

---
+++ app/ui/header/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return(
+    <div className="flex w-full absolute top-0 bg-blue-950 px-2 py-4">
+      
+      <h1 className="text-white">Header</h1>
+    </div>
+  )
+}
